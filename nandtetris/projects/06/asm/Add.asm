de27540dcbd762e4e6f762b9b47fa173735808d8
Eric Orr <Eric.Orr.Dev@gmail.com>
2024-12-03 14:10:38 -0800

    Somehow I filled Add.asm with gibberous. Replaced it with the proper code, outputs now match

--- nandtetris/projects/06/asm/Add.asm
+++ nandtetris/projects/06/asm/Add.asm
@@ -1,1 +1,12 @@
-000000000000000111101100000010000000000000000001111000000100100000000000000000001110001110000100
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+
+// Computes R0 = 2 + 3  (R0 refers to RAM[0])
+
+@2
+D=A
+@3
+D=D+A
+@0
+M=D
